f4c26ef553153e4f45d44a7fa37a8a8874e65cf4
ShiYC <1256325972@qq.com>
2024-03-13 23:08:03 +0800

    fix:update doubanmovie.py des:更新电影，爬去电影简介

--- doubanmovie.py
+++ doubanmovie.py
@@ -1,18 +1,18 @@
-import requests
 import json
 
-try:
-    url = 'https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&page_limit=50&page_start=0'
-    header = {
+from bs4 import BeautifulSoup
+from Req.Get import getbody
+
+if __name__ == '__main__':
+    base = 'https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&page_limit=50&page_start=0'
+    headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
     }
-    req = requests.get(url, headers = header)
-    req.raise_for_status()
-    obj = json.loads(req.text)
+    obj = json.loads(getbody(base, headers))
     subjects = obj['subjects']
     for i in subjects:
         print(i['title'])
-        # print(i['title'] + '    ' + 'https://im1907.top/?jx=' + i['title'])
-
-except requests.RequestException as e:
-    print(e)
+        body = getbody(i['url'], headers)
+        soup = BeautifulSoup(body, features='html.parser')
+        content = soup.find('div', id='link-report-intra').text.strip()
+        print(content)
