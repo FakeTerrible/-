5cf6dbdc192a135193dc2b94ceb9761c7acb0c99
ShiYC <1256325972@qq.com>
2024-03-31 23:29:21 +0800

    fix:更新使用方法和豆瓣电视剧

--- Req/Get.py
+++ Req/Get.py
@@ -4,7 +4,7 @@ def getbody(url, headers):
     try:
         req = requests.get(url, headers=headers)
         req.raise_for_status()
-        print("请求结果：" + str(req.status_code))
+        # print("请求结果：" + str(req.status_code))
         return req.text
     except requests.RequestException as e:
         print(e)
--- doubanpop.py
+++ doubanpop.py
@@ -1,22 +1,26 @@
 import json
 import requests
+from Req.Get import getbody
+from bs4 import BeautifulSoup
 
-try:
+if __name__ == '__main__':
     url = 'https://m.douban.com/rexxar/api/v2/tv/recommend?refresh=0&start=0&count=20&selected_categories=%7B%7D&uncollect=false&tags=2024'
     headers = {
         "Host": "m.douban.com",
         "Origin": "https://movie.douban.com",
         "Referer": "https://movie.douban.com/tv/",
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
-    req = requests.get(url, headers=headers)
-    req.raise_for_status()
-    req.encoding = 'utf-8'
-    obj = json.loads(req.text)
+    headers_1 = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
+    obj = json.loads(getbody(url, headers))
     for i in obj['items']:
         print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
         print("|+" + i['title'])
         print("|+" + i['card_subtitle'])
         print("|+" + "https://im1907.top/?jx=" + i['title'])
-except requests.RequestException as e:
-    print(e)
+        urlson = 'https://movie.douban.com/subject/' + i['id'] + '/'
+        soup = BeautifulSoup(getbody(urlson, headers_1), features='html.parser')
+        report = soup.find('div', id='link-report-intra')
+        print("|+" + report.text.strip())
